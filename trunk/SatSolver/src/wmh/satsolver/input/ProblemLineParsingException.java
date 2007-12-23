ef7e2673e0c0f6084b1579ddd2cec607ab56aa55
docent <docent@2356dd48-353e-0410-baff-fb52aa316383>
2007-12-23 11:05:15 +0000

    porzadek robie ;p

--- trunk/SatSolver/src/wmh/satsolver/input/ProblemLineParsingException.java
+++
@@ -1,7 +0,0 @@
-package wmh.satsolver.input;
-
-public class ProblemLineParsingException extends Exception {
-    public ProblemLineParsingException(String message) {
-        super(message);
-    }
-}
